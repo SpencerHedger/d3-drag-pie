704afeebf4df3208af1bda870a4dfb4d599fb5de
Spencer Hedger <spencer@spencerhedger.com>
2019-10-11 07:06:55 +0100

    First commit

---
+++ js/d3-drag-pie.js
@@ -0,0 +1,112 @@
+function d3dp(config) {
+    function create(data, domTarget) {
+        var _data = data;
+        var _chart = null;
+        var _chart_g = null;
+
+        var _innerRadius = 0;
+        var _outerRadius = 100;
+        var _cornerRadius = 5;
+
+        var _color = d3.scaleOrdinal()
+            .range(config.categoryColors);
+
+        // Pie Segment helper.
+        var pieGenerator = d3.pie()
+            .value(d => d.value)
+            .sort((a, b) => a.name.localeCompare(b.name));
+    
+        // Arc calculation helper.
+        var arcGenerator = d3.arc()
+            .innerRadius(_innerRadius)
+            .outerRadius(_outerRadius)
+            .cornerRadius(_cornerRadius);
+
+        function init() {
+            // Create SVG object.
+            _chart = document.createElementNS("http://www.w3.org/2000/svg", 'svg'); //Create a path in SVG's namespace
+            _chart.setAttribute('width', '500');
+            _chart.setAttribute('height', '500');
+
+            _chart_g = document.createElementNS("http://www.w3.org/2000/svg", 'g'); //Create a path in SVG's namespace
+            _chart_g.setAttribute('transform', 'translate(150,250)');
+
+            _chart.appendChild(_chart_g);
+            domTarget.appendChild(_chart);
+        }
+
+        function draw() {
+            var arcData = pieGenerator(_data);
+
+            // Select.
+            var n = d3.select(_chart_g)
+                .selectAll('path')
+                .data(arcData);
+
+            // Enter.
+            n.enter()
+                .append('path')
+                .attr('d', arcGenerator)
+            
+            // Update.
+            n.attr('d', arcGenerator);
+
+            // Exit.
+            n.exit().remove();
+
+            // Categories in segments
+            for(var i = 0; i < arcData.length; i++) {
+                var seg = arcData[i];
+                var catPaths = [];
+
+                var catArc = d3.arc()
+                    .startAngle(seg.startAngle)
+                    .endAngle(seg.endAngle)
+                    .innerRadius(_innerRadius);
+
+                for(var j = 0; j < seg.data.categories.length; j++) {
+                    catPaths.push({
+                        category: seg.data.categories[j],
+                        path: catArc({ outerRadius: _innerRadius + seg.data.categories[j].value })
+                    });
+                }
+
+                // Sort by descending size so smaller ones are not hidden by bigger ones.
+                catPaths.sort((a, b) => d3.descending(a.category.value, b.category.value));
+
+                // Select.
+                var c = d3.select(_chart_g)
+                    .selectAll('.segcats' + i)
+                    .data(catPaths);
+                
+                // Enter.
+                c.enter()
+                    .append('path')
+                    .attr('d', x => x.path)
+                    .attr('class', 'segcats' + i)
+                    .style('fill', (x, idx) => _color(idx));
+
+                // Update.
+                c.attr('d', x => x.path);
+
+                // Exit.
+                c.exit().remove();
+            }
+
+            return _chart;
+        }
+
+        // Initial display.
+        init();
+        draw();
+
+        // Object to provide external functionality.
+        return {
+            refresh: draw
+        };
+    }
+
+    return {
+        create: create
+    }
+}
